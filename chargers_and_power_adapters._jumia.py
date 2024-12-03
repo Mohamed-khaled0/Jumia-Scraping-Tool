ef6f69fb2247eff80fef5df9051e4a54cda0dacd
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-12-03 20:26:19 +0200

    Update fans_cooling_jumia_products

--- chargers_and_power_adapters._jumia.py
+++ chargers_and_power_adapters._jumia.py
@@ -22,21 +22,21 @@ base_url = 'https://www.jumia.com.eg/mobile-phone-accessories-cables/?page={}'
 
 # Function to get product data from each product div
 def get_product_data(product):
+    # Extract product name
     name = product.find('h3', {'class': 'name'}).get_text(strip=True)
-    
-    # Extracting product details like price, old price, and discount
+
+    # Extract price
     price = product.find('div', {'class': 'prc'}).get_text(strip=True)
-    old_price = product.find('div', {'class': 'old'})
-    if old_price:
-        old_price = old_price.get_text(strip=True)
-    else:
-        old_price = None
-    discount = product.find('div', {'class': 'bdg _dsct _sm'})
-    if discount:
-        discount_percentage = discount.get_text(strip=True)
-    else:
-        discount_percentage = None
-    
+
+    # Extract product link (adjusted to get correct href)
+    link_tag = product.find('a', {'class': 'core'})
+    link = 'https://www.jumia.com.eg' + link_tag['href'] if link_tag else None
+
+    # Extract product image URL (adjusted to get data-src)
+    img_tag = product.find('img', {'class': 'img'})
+    img_url = img_tag['data-src'] if img_tag and 'data-src' in img_tag.attrs else None
+
+
     # Determine the category (brand) based on product name
     category = None
     for brand in brands:
@@ -49,10 +49,11 @@ def get_product_data(product):
     return {
         'Product Name': name,
         'Price': price,
-        'Old Price': old_price,
-        'Discount Percentage': discount_percentage,
-        'Category': category  # Added the category (brand)
+        'Product Link': link,
+        'Image URL': img_url,
+        'Category': category
     }
+
 
 # Function to scrape products from a given page
 def scrape_page(page_num):
